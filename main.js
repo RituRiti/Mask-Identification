8aacbb2ef0a8777500d9383b3c20ca778dc5eeec
RituRiti <135221215+RituRiti@users.noreply.github.com>
2023-06-04 12:05:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function setup(){
     video.hide();
     canvas = createCanvas(300,300);
     canvas.center();
+    console.log('ml5 version:',ml5.version);
     classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/I4f7OVWck/model.json',modelLoaded);
 
 }
